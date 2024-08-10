c5226c35661c8b3a659b211521752b52d98441fb
Stephen Muema <muema1476@gmail.com>
2024-08-10 08:15:35 +0300

    Added javascript

--- main.js
+++ main.js
@@ -18,7 +18,8 @@ function showTime(){
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
     
-    var time = h + ":" + m + ":" + s + " " + session;
+ var time = h + ":" +" hours "  +  m + ": "+"  min " + s + " sec " + session;
+
     document.getElementById("MyClockDisplay").innerText = time; 
     document.getElementById("MyClockDisplay").textContent = time;
     
